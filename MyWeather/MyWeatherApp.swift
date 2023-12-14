86914d66c4130994edf79aaee60b478c92babab6
Yingxue <yingxue0323@gmail.com>
2023-12-14 16:45:02 +1100

    Initial Commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by 李映雪 on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
